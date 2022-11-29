71ae3c06e81ea67a702d676d06f6cbfd4d0ceeb1
liliia87 <lilia.krivelyova@gmail.com>
2022-11-28 17:10:59 -0800

    additional files

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import '../App.css';
+import React from 'react';
+import TopStories from './TopStories';
+
+function App() {
+  return (
+    <React.Fragment>
+      < TopStories />
+    </React.Fragment>
+  );
+}
+
+export default App;
